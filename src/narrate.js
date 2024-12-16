86fe7e8519845fd6556d5129ee7ff95d69810cd4
Jan Miksovsky <jan@miksovsky.com>
2024-12-16 09:11:10 -0800

    Add padding to file numbers to make sorting easier

--- src/narrate.js
+++ src/narrate.js
@@ -1,16 +1,10 @@
 import { Tree } from "@weborigami/async-tree";
 import tts from "./tts.js";
 
-const voices = {
-  Alice: "shimmer",
-  Bob: "echo",
-};
-
 // Function to call the OpenAI Text-to-Speech API
 export default async function narrate(stageDirection) {
   const data = await Tree.plain(stageDirection);
-  const actor = Object.keys(data)[0];
-  const voice = voices[actor];
+  const voice = Object.keys(data)[0];
   const text = data[voice];
   return tts.call(this, text, voice);
 }
