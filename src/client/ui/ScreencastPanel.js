23c6c17588a277c9eadf949548b7f60ed3d55f57
Jan Miksovsky <jan@miksovsky.com>
2025-01-16 15:20:00 -0800

    Back/Forward buttons

--- src/client/ui/ScreencastPanel.js
+++ src/client/ui/ScreencastPanel.js
@@ -233,7 +233,7 @@ export default class ScreencastPanel extends SoundMixin(
 }
 
 function raiseSoundChangeEvent(target, sound) {
-  console.log("raiseSoundChangeEvent", sound);
+  // console.log("raiseSoundChangeEvent", sound);
   target.dispatchEvent(
     new CustomEvent("sound-change", {
       bubbles: true,
